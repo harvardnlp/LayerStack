dbc83f9a36b48db2476d3848e2f25924bcc79a1b
srush <srush.research@gmail.com>
2019-07-29 22:01:59 +0000

    .

--- code.js
+++ code.js
@@ -1,27 +1,41 @@
 parser = require("./parser.js");
 Raphael = require("raphael")
 parser.parse("A/S@10")
-
+// Creates canvas 320 × 200 at 10, 50
+var paper = Raphael(200, 200, 320, 200);
 function run() {
     var out = parser.parse(document.getElementById("area").value);
-
-    // Creates canvas 320 × 200 at 10, 50
-    var paper = Raphael(200, 200, 320, 200);
-
-    function draw(stack, x, y){
+    paper.clear();
+    function draw(stack, x, y, dx, dy, rotate){
         if (Array.isArray(stack)){
-            for (var i = 0; i < stack.length(); ++i) {
-                ne = draw(stack[i], x, y);
+            for (var i = 0; i < stack.length ;++i){
+                ne = draw(stack[i], x , y, dx, dy, rotate);
                 x = ne[0];
                 y = ne[1];
             }
+            return [x, y];
         }
-        if (isString(stack)) {
-            paper.text(x, y, stack);
-            return [x, y+10];
+        if (typeof stack === 'string' || stack instanceof String){
+            text2 = paper.rect(x+dx-20, y+dy-5 ,40, 10);
+            text = paper.text(x+dx, y+dy, stack);
+
+            text.rotate(rotate);
+            text2.rotate(rotate);
+
+            return [x, y+30];
         }
+        if (stack["op"] == "rotate") {
+            rotate += stack["value"];
+            return draw(stack["child"], x, y, dx, dy, rotate);
+        }
+        if (stack["op"] == "shift") {
+            dx += stack["value"][0];
+            dy += stack["value"][1];
+            return draw(stack["child"], x, y, dx, dy, rotate);
+        }
+
     }
-    draw(out, 0, 0);
+    draw(out, 100, 10, 0, 0, 0);
 
 
 }
